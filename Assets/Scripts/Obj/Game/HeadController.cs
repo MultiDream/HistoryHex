0289416b75fa6db0d0b94af462233b3e766b1c12
MultiDream <luka.derado@gmail.com>
2019-09-15 17:48:37 -0400

    Testing new .gitignore file
    Uploading a project, and asking a friend to pull down the project and run it out of the box. Success will determine merge with master.

---
+++ Assets/Scripts/Obj/Game/HeadController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Master Class for the game.
+/// </summary>
+public class HeadController : MonoBehaviour
+{
+	public GameObject[] playerPrefabs;
+	public GameObject UI;
+
+	public int activePlayer = 0;
+	public KeyboardController keyboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+		keyboard.BindKey(KeyCode.K,K_Press);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	// Key Bindings
+
+	/// <summary>
+	/// Changes the active player.
+	/// </summary>
+	public void K_Press(){
+		Debug.Log("K Key Pressed!");
+		if (activePlayer < playerPrefabs.Length - 1){
+			activePlayer++;
+		} else {
+			activePlayer = 0;
+		}
+
+		//Set UI flag color.
+		UI.GetComponent<UIMaster>().ActivePlayer = playerPrefabs[activePlayer].GetComponent<Player>();
+		return;
+	}
+}
