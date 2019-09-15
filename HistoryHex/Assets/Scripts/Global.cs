d2d40d3c7dcf09e267a895d24a96f37d577bcd5a
MultiDream <luka.derado@gmail.com>
2019-09-14 20:44:52 -0400

    Pushing Project into repo.

---
+++ HistoryHex/Assets/Scripts/Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds a number of global project settings.
+/// </summary>
+public static class Global
+{
+	public static float MAXIMUM_FOOD = 4.0f;
+	public static int MAX_PLAYERS = 4;
+}
---
+++ HistoryHex/Assets/Scripts/UI/UIMaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script's purpose is to manage a series of other UI components.
+///	For now, that list includes:
+///		1. The Selection Controller
+///		2. The Active Player Display.
+///		3. The Game Timer.
+///		
+/// This class isn't ready yet, but eventually all components will be moved here.
+/// </summary>
+public class UIMaster : MonoBehaviour
+{
+	public ILedger activeLedger;
+	public Player ActivePlayer {
+		set {
+			Debug.Log(value.Colour);
+			transform.GetChild(3).GetComponent<Image>().color = value.Colour;
+		} 
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make sure the correct active player is ready.
+    }
+}
