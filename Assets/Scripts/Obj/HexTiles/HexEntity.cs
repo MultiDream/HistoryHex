ef57768673c6fa83326a927be59ab79d26c46784
Daniel Keehn <40642338+DanielKeehn@users.noreply.github.com>
2019-10-10 02:53:44 -0400

    Updated HexEntity
    The population now is initialized when the map is created, and food and population are updated every turn

--- Assets/Scripts/Obj/HexTiles/HexEntity.cs
+++ Assets/Scripts/Obj/HexTiles/HexEntity.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,10 @@ public class HexEntity : MonoBehaviour
 	#region Properties
 	// Internal variables
 	private bool activated = false;
+	// This variable keeps track of how many turns have passed
+	private int turnCounter = 0;
+	// This variable calculates if an attack was made on this tile after your previous turn
+	private bool attacked = false;
 
 	//Prefabs
 	public GameObject ArmyPrefab;
@@ -18,10 +22,8 @@ public class HexEntity : MonoBehaviour
 	public string Name { get; set; }
 	public Player Controller { get; set; }
 	public EntityDrawer drawer;
-	public GameObject army; // make into an array later.
-	public int Population;
-	public int Food_Base;
-
+	public GameObject army; // make into an array later, whne multiple armies can sit on a tile.
+	public float Population;
 	// SelectionInterface
 	private SelectableObj SelectionInterface;
 	#endregion
@@ -35,24 +37,7 @@ public class HexEntity : MonoBehaviour
     // Update is called once per frame.
     void Update()
     {
-		// Move the responsibility of setting Map Viewing modes to another class later.
-
-		// Shows the Food Map.
-		if (Input.GetKeyDown(KeyCode.F)){
-			drawer.Color = new Color(Food/4f,0,0);
-		}
-		//Shows the Control Map.
-		else if (Input.GetKeyDown(KeyCode.G)) {
-			if (Controller != null){
-				drawer.Color = Controller.Colour;
-			} else {
-				drawer.Color = Color.black;
-			}
-		}
-		// Clears the map.
-		else if (Input.GetKeyDown(KeyCode.R)) {
-			drawer.Color = Color.white;
-		}
+		MapDrawingUpdater();
 
 		//Attempt to wire the SelectionInterface.
 		SelectionInterface = transform.GetComponent<SelectableObj>();
@@ -79,9 +64,26 @@ public class HexEntity : MonoBehaviour
 			this.army = Instantiate(ArmyPrefab, position, rotation);
 
 			//Set an army up.
-			army.transform.GetComponent<ArmyEntity>().Position = Position;
-			army.transform.GetComponent<ArmyEntity>().Controller = Controller;
+			ArmyEntity armyEntity = army.transform.GetComponent<ArmyEntity>();
+			armyEntity.Position = Position;
+			armyEntity.Controller = Controller;
 			
+		}
+	}
+
+	private void MapDrawingUpdater(){
+		// Shows the Food Map.
+		if (Global.CurrentMapMode == MapMode.Food) {
+			drawer.Color = new Color(Food / 4f, 0, 0);
+		}
+
+		//Shows the Control Map.
+		else if (Global.CurrentMapMode == MapMode.Controller) {
+			if (Controller != null) {
+				drawer.Color = Controller.Colour;
+			} else {
+				drawer.Color = Color.black;
+			}
 		}
 	}
 
@@ -105,17 +107,48 @@ public class HexEntity : MonoBehaviour
 		Name = "NoMansLand";
 		Food = Mathf.Floor(Random.value * Global.MAXIMUM_FOOD);
 		drawer = new EntityDrawer(transform.GetChild(0));
+		InitializePopulation();
+	}
+
+	// Runs during initialization of tiles and sets a base population for each tile
+	// The base population now is equal to base food but this can be changed as more is implemented 
+	private void InitializePopulation() {
+		// No food means that no population can be created
+		if (Food >= 0) {
+			Population = Food;
+		}
+	}
+
+	// This updates every turn and decides if updatePopulation() is run
+	private void checkUpdatePopulation() {
+		if (turnCounter >= 4 && Food >= 0) {
+			updatePopulation();
+			turnCounter = 0;
+		}
+	}
+
+	// This updates the Population variables 
+	private void updatePopulation() {
+		Population++;
+	}
+
+	// This runs every turn and updates food based on population if not attacked after previous turn
+	// The amount of Food added every turn simply equals the population but this can be changed as more is implemented
+	private void updateFood() {
+		if (Food >= 0 & attacked == false) {
+			Food += Population;
+		}
+	}
+
+	// This runs after every turn run by the Controlling Player
+	public void updateTurn() {
+		updateFood();
+		checkUpdatePopulation();
+		turnCounter++;
 	}
 
 	//Draw Delegation
 	private void Draw() {
 		drawer.Update();	
-	}
-
-	//This function updates how much food is added to a tile based on the 
-	//Food Base (all the people in a population working to make food)
-	public void populationFoodProduction(int foodBase) {
-		//Right now one person = one food to make things simple, but this value can be changed as gameplay is tested
-		Food+= foodBase;
 	}
 }
